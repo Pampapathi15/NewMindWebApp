4f81e7ea5e44ef793f265941ca02ab6e226f33d7
Pampapathi <Pampapathi.kudlur@renewin.com>
2022-09-13 00:12:59 +0530

    Socia Login

---
+++ NewMindWebAPPV1/SocialGmail/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialGmail.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
