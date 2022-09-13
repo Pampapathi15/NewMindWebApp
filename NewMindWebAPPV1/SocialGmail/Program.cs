ca333e344973b9d0143bd823991223865d091cb3
Pampapathi <Pampapathi.kudlur@renewin.com>
2022-09-13 10:30:01 +0530

    Social Login Changes Coonection in Program.Cs and Appsetting.json

--- NewMindWebAPPV1/SocialGmail/Program.cs
+++ NewMindWebAPPV1/SocialGmail/Program.cs
@@ -3,6 +3,16 @@ using Microsoft.EntityFrameworkCore;
 using SocialGmail.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+
+services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+});
+
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
